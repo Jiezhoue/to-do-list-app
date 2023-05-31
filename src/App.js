2bf025509c59d62e99a1eb360b84d85424b6a676
Jiezhoue <13809@coderacademy.edu.au>
2023-05-31 12:58:40 +1000

    create skeleton of the to do list data
    create ListContext to use as a global state cross all pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<h1>HomePage</h1>} />
+        <Route path='/add' element={<h1>FormPage</h1>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/contexts/ListContext.jsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useReducer } from "react";
+
+const initialListData = [
+  { id: 1, 
+    title: "Create a to do list app", 
+    content: "Install react app first....",
+    isFinish: false,
+    create_date: Date.now()
+  }
+]
+
+const listReducer = (previousState, instructions) => {
+  let stateEditable = [...previousState];
+  switch(instructions.type) {
+    case 'add':
+      console.log("add");
+      break;
+    case 'update':
+      console.log('update');
+      break;
+    case 'delete':
+      console.log('delete')
+      break;
+    default:
+      return previousState;
+  }
+}
+
+export const ListDataContext = createContext(null);
+export const ListDispatchContext = createContext(null);
+
+export function useListData() {
+  return useContext(ListDataContext)
+}
+
+export function useListDispatch() {
+  return useContext(ListDispatchContext)
+}
+
+export default function ListProvider(props) {
+  const [listData, listDispatch] = useReducer(listReducer, initialListData)
+
+  return (
+    <ListDataContext.Provider value={listData}>
+      <ListDispatchContext.Provider value={listDispatch}>
+        {props.children}
+      </ListDispatchContext.Provider>
+    </ListDataContext.Provider>
+  )
+}
